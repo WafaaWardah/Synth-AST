294e49d88c011bed7bb9da489acb328332363585
Wafaa Wardah <wafaawardah89@gmail.com>
2026-02-03 18:40:02 +0100

    Fix typos in comments and docstrings in models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # ASTXL Model: A PyTorch model using the pre-trained AST from MIT for audio feature prediction. 
-# This is intended for pricting one score in the range 0-1.
+# This is intended for predicting one score in the range 0-1.
 
 import torch
 import torch.nn as nn
@@ -8,7 +8,6 @@ from transformers import ASTModel, logging
 logging.set_verbosity_error()  # Silence unnecessary warnings
 
 class ASTXL(nn.Module):
-    """Model-XL with individual AST for each dimension."""
 
     PRETRAINED_MODEL = "MIT/ast-finetuned-audioset-10-10-0.4593"
 
@@ -25,7 +24,6 @@ class ASTXL(nn.Module):
         )
 
     def forward(self, features: torch.Tensor) -> torch.Tensor:
-        """Forward pass for ASTXL."""
         hidden_state = self.ast(features).pooler_output
         pred = self.fc(hidden_state).squeeze()
         return pred
